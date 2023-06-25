774d90db75e5613089ed4b0e99e214eb192b4ed1
julijaj <123488334+julijaj@users.noreply.github.com>
2023-06-25 13:39:50 +0300

    refactored component styles

--- src/pages/trips/[id].js
+++ src/pages/trips/[id].js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Head from 'next/head';
-import styles from '../../styles/Home.module.css';
+import styles from "./styles.module.css";
 import { useRouter } from 'next/router'
 
 export default function Trip() {
@@ -67,45 +67,6 @@ export default function Trip() {
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      
     </div>
   )
 }
