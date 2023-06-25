10551395daa6a7c2d735859da081e85d343e0ab4
julijaj <123488334+julijaj@users.noreply.github.com>
2023-06-25 13:14:30 +0300

    refactored global styles

--- src/pages/insertTrip.js
+++ src/pages/insertTrip.js
@@ -82,20 +82,6 @@ export default function Home() {
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
