1324a0e0fe50733749c148bc2ecbb82aaff1750d
julijaj <123488334+julijaj@users.noreply.github.com>
2023-06-28 19:17:38 +0300

    /insertTrip page created

--- src/pages/insertTrip/index.js
+++ src/pages/insertTrip/index.js
@@ -1,31 +1,10 @@
 import styles from "./styles.module.css";
+import TripForm from 'src/components/tripForm/tripForm'
 
 export default function Home() {
   return (
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Mexico!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a
-            href="/"
-            className={styles.card}
-          >
-            <h3>Back &rarr;</h3>
-          </a>
-
-          <a
-            href="#"
-            className={styles.card}
-          >
-            <h3>Create &rarr;</h3>
-          </a>
-        </div>
+        <TripForm />
       </main>
   )
 }
